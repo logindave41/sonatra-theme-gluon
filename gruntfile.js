45f9164af836cef6a9ce56e4ad2a3134adb1d43b
François Pluchino <francois.pluchino@sonatra.com>
2015-01-02 15:05:01 +0100

    Extract footable striped stylesheet

--- gruntfile.js
+++ gruntfile.js
@@ -23,6 +23,7 @@ module.exports = function (grunt) {
                     "css/bootstrap.css": "less/bootstrap/bootstrap.less",
                     "css/bootstrap-theme.css": "less/bootstrap-theme/bootstrap-theme.less",
                     "css/footable.css": "less/footable/footable.less",
+                    "css/footable-striped.css": "less/footable-striped/footable-striped.less",
                     "css/sonatra-bootstrap-panel-collapse.css": "less/bootstrap-panel-collapse/bootstrap-panel-collapse.less",
                     "css/sonatra-bootstrap-nav-scroll.css": "less/bootstrap-nav-scroll/bootstrap-nav-scroll.less",
                     "css/sonatra-jquery-datetime-picker.css": "less/jquery-datetime-picker/jquery-datetime-picker.less",
@@ -32,8 +33,7 @@ module.exports = function (grunt) {
                     "css/sonatra-select2-responsive.css": "less/select2-responsive/select2-responsive.less",
                     "css/sonatra-jquery-table-select.css": "less/jquery-table-select/jquery-table-select.less",
                     "css/sonatra-jquery-table-pager.css": "less/jquery-table-pager/jquery-table-pager.less",
-                    "css/gluon.css": "less/gluon/gluon.less",
-                    "css/components.css": "less/components.less"
+                    "css/gluon.css": "less/gluon/gluon.less"
                 }
             }
         }
