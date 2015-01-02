d168e23bfb3f9b5f297b41b0cbed9b03e4b5c8ed
François Pluchino <francois.pluchino@sonatra.com>
2015-01-02 14:03:20 +0100

    Move bootstrap-theme less file

--- gruntfile.js
+++ gruntfile.js
@@ -21,7 +21,7 @@ module.exports = function (grunt) {
                 },
                 files: {
                     "css/bootstrap.css":       "less/bootstrap/bootstrap.less",
-                    "css/bootstrap-theme.css": "less/bootstrap-theme.less",
+                    "css/bootstrap-theme.css": "less/bootstrap-theme/bootstrap-theme.less",
                     "css/components.css":      "less/components.less"
                 }
             }
