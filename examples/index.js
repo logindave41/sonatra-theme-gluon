b9aee9f321f5e5d7eb13be2b96a1eb6900ddfaf9
François Pluchino <francois.pluchino@sonatra.com>
2015-08-31 15:52:13 +0200

    Add table card example with footable and select

--- examples/index.js
+++ examples/index.js
@@ -39,7 +39,7 @@ function ajaxTable(url, req, res) {
                 'firstname': 'First name ' + i,
                 'lastname': 'Last name ' + i,
                 'username': 'Username ' + i,
-                '_selectable': '<input type="checkbox">'
+                '_selectable': '<div class="checkbox"><label class="form-control-fa"><input type="checkbox" class="input-secondary"><i class="fa form-control-fa-style"></i></label></div>'
             });
         }
 
