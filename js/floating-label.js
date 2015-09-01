3660de5db94262401423d46a13b7995379863eb4
François Pluchino <francois.pluchino@sonatra.com>
2015-09-01 13:18:39 +0200

    Add floating label style

---
+++ js/floating-label.js
@@ -0,0 +1,172 @@
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*global define*/
+/*global jQuery*/
+/*global window*/
+
+/**
+ * @param {jQuery} $
+ */
+(function (factory) {
+    'use strict';
+
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
+    'use strict';
+
+    /**
+     * Display the floating label.
+     *
+     * @param {jQuery.Event|Event} event
+     *
+     * @private
+     */
+    function onFocusOut(event) {
+        var $target = $(event.currentTarget);
+
+        if ('' === $target.val()) {
+            $target.removeClass('has-floating-content');
+        } else {
+            $target.addClass('has-floating-content');
+        }
+    }
+
+    // RIPPLE CLASS DEFINITION
+    // =======================
+
+    /**
+     * @constructor
+     *
+     * @param {string|elements|object|jQuery} element
+     * @param {object}                        options
+     *
+     * @this FloatingLabel
+     */
+    var FloatingLabel = function (element, options) {
+        this.guid     = $.guid;
+        this.options  = $.extend(true, {}, FloatingLabel.DEFAULTS, options);
+        this.$element = $(element);
+
+        if ('' === this.options.floatingLabelSelector) {
+            this.options.floatingLabelSelector = null;
+        }
+
+        this.$element.on('focusout.st.floating-label' + this.guid, this.options.floatingLabelSelector, this, onFocusOut);
+
+        var $targets = null !== this.options.floatingLabelSelector ? $(this.options.floatingLabelSelector, this.$element)
+            : this.$element;
+
+        $targets.each(function (index) {
+            var $target = $targets.eq(index),
+                $label = $('> label', $target),
+                placeholder = $target.attr('placeholder');
+
+            $target.after('<span class="floating-bar"></span>');
+
+            if (0 === $label.length && placeholder) {
+                $target.removeAttr('placeholder');
+                $target.after('<label>' + placeholder + '</label>');
+            }
+        });
+    },
+        old;
+
+    /**
+     * Defaults options.
+     *
+     * @type Array
+     */
+    FloatingLabel.DEFAULTS = {
+        floatingLabelSelector: null
+    };
+
+    /**
+     * Destroy instance.
+     *
+     * @this FloatingLabel
+     */
+    FloatingLabel.prototype.destroy = function () {
+        var $targets = null !== this.options.floatingLabelSelector ? $(this.options.floatingLabelSelector, this.$element)
+                : this.$element;
+
+        this.$element.off('focusout.st.floating-label' + this.guid, this.options.floatingLabelSelector, onFocusOut);
+
+        $targets.each(function (index) {
+            var $target = $targets.eq(index),
+                $label = $('> label', $target.parent());
+
+            if ($label.length > 0) {
+                $target.attr('placeholder', $label.text());
+                $label.remove();
+            }
+
+            $('> .floating-bar', $target.parent()).remove();
+            $target.removeClass('has-floating-content');
+        });
+    };
+
+
+    // RIPPLE PLUGIN DEFINITION
+    // ========================
+
+    function Plugin(option, value) {
+        return this.each(function () {
+            var $this   = $(this),
+                data    = $this.data('st.floating-label'),
+                options = typeof option === 'object' && option;
+
+            if (!data && option === 'destroy') {
+                return;
+            }
+
+            if (!data) {
+                data = new FloatingLabel(this, options);
+                $this.data('st.floating-label', data);
+            }
+
+            if (typeof option === 'string') {
+                data[option](value);
+            }
+        });
+    }
+
+    old = $.fn.floatingLabel;
+
+    $.fn.floatingLabel             = Plugin;
+    $.fn.floatingLabel.Constructor = FloatingLabel;
+
+
+    // RIPPLE NO CONFLICT
+    // ==================
+
+    $.fn.floatingLabel.noConflict = function () {
+        $.fn.floatingLabel = old;
+
+        return this;
+    };
+
+
+    // RIPPLE DATA-API
+    // ===============
+
+    $(window).on('load', function () {
+        $('[data-floating-label]').each(function () {
+            var $this = $(this);
+            Plugin.call($this, $this.data());
+        });
+    });
+
+}));
