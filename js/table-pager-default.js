deae668c186d6fa4867ddc8676c16e46a8102ac4
François Pluchino <francois.pluchino@sonatra.com>
2015-09-07 03:09:05 +0200

    Add floating spinner for table pager

---
+++ js/table-pager-default.js
@@ -0,0 +1,38 @@
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*global define*/
+/*global jQuery*/
+
+/**
+ * @param {jQuery} $
+ *
+ * @typedef {object} define.amd
+ */
+(function (factory) {
+    'use strict';
+
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery', 'sonatra-jquery-table-pager'], factory);
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
+    'use strict';
+
+    // TABLE PAGER CLASS DEFINITION
+    // ============================
+
+    $.fn.tablePager.Constructor.DEFAULTS = $.extend(true, {}, $.fn.tablePager.Constructor.DEFAULTS, {
+        loadingTemplate: '<caption><div class="spinner-floating spinner-mini"><svg class="spinner spinner-accent"><circle class="spinner-path" cx="22" cy="22" r="20"></circle></svg></div></caption>'
+    });
+
+}));
